fe070c428ac07fda048c88e2d1e8622239a2e783
Georgette Radagast <gradagas@va-c3.kzn.21-school.ru>
2021-12-06 21:10:24 +0300

    06.12

--- get_next_line.c
+++ get_next_line.c
@@ -10,4 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char *get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	char *str;
+
+	str = (char *)malloc(BUFFER_SIZE + 1);
+	if (!str)
+	return (NULL);
+
+	read(fd, str, BUFFER_SIZE);
+
+/*
+	char c;
+	while (read(fd, &c, 1) && c != '\n')
+		write(1, &c, 1);
+*/
+	return (str);
+}

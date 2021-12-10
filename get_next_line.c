7c5432b9798ad7d84ea83ce3f683a3b0dd4e62f7
Georgette Radagast <gradagas@va-h5.kzn.21-school.ru>
2021-12-10 19:51:48 +0300

    10.12v2

--- get_next_line.c
+++ get_next_line.c
@@ -21,25 +21,33 @@
 
 
 
-int	init_str_buf(char *str_buff)
+int	init_str_buf(char **str_buff, size_t begin_buff_size, char *buffer)
 {
-	static char	buffer[BUFFER_SIZE + 1];
 	int have_str_buff;
-	size_t buff_size;
 
+	if (!begin_buff_size)
+	{
+		*str_buff = (char *)malloc(BUFFER_SIZE + 1);
+		if (!*str_buff)
+			return (0);
+	}
+	else
+	{
+		*str_buff = (char *)malloc(BUFFER_SIZE + begin_buff_size + 1);
+		if (!*str_buff)
+			return (0);
 
-	buff_size = ft_strlen(buffer);
-	if (!buff_size)
-		buff_size = BUFFER_SIZE;
+		printf("str_buff = %s\n", *str_buff);
+		printf("buffer contain = %s\n", buffer);
+		printf("begin_buff_size = %lu\n", begin_buff_size);
 
-	str_buff = (char *)malloc(buff_size + 1);
-	if (!str_buff)
-		return (0);
-	printf("make str_buf\n");
+		ft_memcpy(*str_buff, buffer, begin_buff_size);
 
-	ft_memcpy(str_buff, buffer, buff_size);
-	printf("str_buff1 = %s\n", str_buff);
+		printf("str_buff = %s\n", *str_buff);
 
+	}
+	printf("made str_buf\n");
+	printf("str_buff = %s\n", *str_buff);
 
 	have_str_buff = 1;
 	return (have_str_buff);
@@ -54,29 +62,29 @@ int	init_str_buf(char *str_buff)
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
-	char		*str_buff;
 	char 		*str_ret;
+	char		*str_buff;
 	int			i;
 	int			have_str_buff;
-	char *		find_end;
-	char *		find_enter;
+	char 		*find_end;
+	char 		*find_enter;
 	size_t		len_buff_str;
 	size_t		len_str;
+	size_t		begin_buff_size;
 
-	i = 1;
-	have_str_buff = 0;
-	find_end = 0;
-	find_enter = 0;
+	printf("*** start gnl ***\n");
 
+	i = 0;
+	find_end = NULL;
+	find_enter = NULL;
 
-	if (ft_strlen(buffer))
-	{
-		have_str_buff = init_str_buf(str_buff);
-		if (!have_str_buff)
-			return (NULL);
-	}
-
-
+	begin_buff_size = ft_strlen(buffer);
+	printf("0 begin_buff_size = %lu\n", begin_buff_size);
+	printf("buffer contain = %s\n", buffer);
+	have_str_buff = init_str_buf(&str_buff, begin_buff_size, buffer);
+	if (!have_str_buff)
+		return (NULL);
+	printf("2 str_buff = %s\n", str_buff);
 	while(!find_enter && !find_end)
 	{
 		read(fd, buffer, BUFFER_SIZE);
@@ -86,24 +94,21 @@ char	*get_next_line(int fd)
 
 		find_enter = (char *)ft_memchr(buffer, '\n', BUFFER_SIZE);
 
-			if(!have_str_buff)
-			{
-				have_str_buff = init_str_buf(str_buff);
-				if (!have_str_buff)
-					return (NULL);
-			}
-			else
-			{
-//				str_buff = ft_realloc(str_buff, BUFFER_SIZE * i + 1,
-//									  (BUFFER_SIZE * (i + 1)) + 1);
-				str_buff = ft_realloc(str_buff, BUFFER_SIZE * i + 1,
-									  (BUFFER_SIZE * (i + 1)) + 1);
 
-				ft_memcpy(&str_buff[BUFFER_SIZE * i], buffer, BUFFER_SIZE);
-				printf("str_buff2 = %s\n", str_buff);
-				i++;
+		if (i == 0)//first iter
+		{
+			printf("i == 0\n");
+			ft_memcpy(&str_buff[begin_buff_size], buffer, BUFFER_SIZE + 1);
+		}
+		else //next iter
+		{
+			str_buff = ft_realloc(str_buff, BUFFER_SIZE * i + 1 + begin_buff_size,
+								  BUFFER_SIZE * (i + 1) + 1 + begin_buff_size);
 
-			}
+			ft_memcpy(&str_buff[BUFFER_SIZE * i + begin_buff_size], buffer, BUFFER_SIZE + 1);
+			printf("str_buff2 = %s\n", str_buff);
+		}
+		i++;
 		printf("-----------------------------\n");
 	}
 
@@ -113,34 +118,30 @@ char	*get_next_line(int fd)
 
 
 	//don't forget '\0' in an and (1 line in file)
-	len_str = (char *)ft_memchr(str_buff, '\n', BUFFER_SIZE * i) - str_buff;
+	if (!find_end)
+		len_str = (char *)ft_memchr(str_buff, '\n', BUFFER_SIZE * i + begin_buff_size) - str_buff;
+	else
+		len_str = (char *)ft_memchr(str_buff, '\0', BUFFER_SIZE * i +
+		begin_buff_size) - str_buff;
 	printf("len_str = %lu\n", len_str);
 
 	str_ret = (char *) malloc(len_str + 1);
 
 	ft_memcpy(str_ret, str_buff, len_str);
-	printf("buffer3 = %s\n", buffer);
+	str_ret[len_str] = '\0';
 
-	//don't forget '\0' in an and (1 line in file)
+	printf("buffer3 = %s\n", buffer);
 	len_buff_str = (char *) ft_memchr(buffer, '\n', BUFFER_SIZE) - buffer;
 	ft_memcpy(buffer, &buffer[len_buff_str + 1], BUFFER_SIZE - len_buff_str);
 	buffer[BUFFER_SIZE - len_buff_str] = '\0';
 
-	printf("buffer4 (fin) = %s\n", buffer);
+	printf("buffer4 (fin) = |%s|\n", buffer);
 	free(str_buff);
-
-
-
-
-
-
+	printf("str_ret len = %lu\n", ft_strlen(str_ret));
+	printf("str_ret = |%s|\n", str_ret);
+	printf("*****************************************\n");
 
 
 	//don't forget free all malloc!!!
-/*
-	char c;
-	while (read(fd, &c, 1) && c != '\n')
-		write(1, &c, 1);
-*/
 	return (str_ret);
 }

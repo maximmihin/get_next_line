1f5b1a8d7567a9e55e3f86e0a0b8af8ea4a08fc9
Amnesia <maksim.mikhin1@gmail.com>
2021-12-10 12:31:32 +0300

    10.12v1

--- get_next_line.h
+++ get_next_line.h
@@ -14,16 +14,11 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define BUFFER_SIZE 1
+#define BUFFER_SIZE 3
 
 size_t	ft_strlen(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strcpy(char *dest, char *src);
 
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 
 char	*ft_realloc(char *buff, int oldlen, int newlen);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -22,82 +22,6 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*arr;
-	size_t	i;
-
-	if (!s1 || !s2)
-		return (0);
-	i = ft_strlen(s1) + ft_strlen(s2);
-	arr = (char *)malloc(sizeof (*s1) * (i + 1));
-	if (!arr)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		arr[i] = s1[i];
-		i++;
-	}
-	while (*s2)
-	{
-		arr[i] = *s2++;
-		i++;
-	}
-	arr[i] = 0;
-	return (arr);
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	d_len;
-	size_t	s_len;
-
-	d_len = 0;
-	while (*dst && d_len < dstsize)
-	{
-		dst++;
-		d_len++;
-	}
-	s_len = ft_strlen(src);
-	if (dstsize <= d_len)
-		return (dstsize + s_len);
-	dstsize -= d_len + 1;
-	while (dstsize && *src)
-	{
-		*dst = *src;
-		dst++;
-		src++;
-		dstsize--;
-	}
-	*dst = '\0';
-	return (d_len + s_len);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s != (unsigned char)c && *s != '\0')
-		s++;
-	if (*s == (unsigned char)c)
-		return ((char *)s);
-	else
-		return (NULL);
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*dest;
@@ -114,33 +38,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		dest[i] = source[i];
 		i++;
 		n--;
-	}
-	return (dst);
-}
-
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	size_t	i;
-
-	if (!dst && !src)
-		return (NULL);
-	if (dst < src)
-	{
-		i = 0;
-		while (i < len)
-		{
-			*(char *)(dst + i) = *(char *)(src + i);
-			i++;
-		}
-	}
-	else
-	{
-		i = (int) len;
-		while (i)
-		{
-			*(char *)(dst + i - 1) = *(char *)(src + i - 1);
-			i--;
-		}
 	}
 	return (dst);
 }

89b756175ec134f2134812d70bd380b0d2b45bf3
Georgette Radagast <gradagas@un-c3.kzn.21-school.ru>
2021-12-20 22:00:57 +0300

    sad

--- get_next_line.h
+++ get_next_line.h
@@ -27,9 +27,7 @@ char	*get_next_line(int fd);
 
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-size_t	ft_strlen(const char *str);
-char	*ft_realloc(char *buff, int oldlen, int newlen);
-
 void	ft_bzero(void *s, size_t n);
+char	*ft_realloc(char *buff, int oldlen, int newlen);
 
 #endif

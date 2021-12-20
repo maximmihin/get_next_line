e49c00bc30ba995774c349f1961e08e493a42784
Georgette Radagast <gradagas@un-c3.kzn.21-school.ru>
2021-12-20 16:22:25 +0300

    ready-to-refactoring

--- get_next_line.c
+++ get_next_line.c
@@ -16,23 +16,19 @@ char 	*buffer_preproc(char *buffer, int *len_str)
 {
 	char 		*str;
 	char 		*find_enter;
+	char		*find_end;
+	int 		len;
 
-//	printf("buffer (pp-s) = %s\n", buffer);
-//	printf("c = %c\n", buffer[BUFFER_SIZE]);
 	if (buffer[BUFFER_SIZE] == '!')
 	{
-//		printf("C 1\n");
 		return (NULL);
 	}
 	//1 and 2
 	find_enter = (char *)ft_memchr(buffer, '\n', BUFFER_SIZE);
 	if (!*buffer || find_enter == &buffer[BUFFER_SIZE - 1])
 	{
-//		printf("CASE 1\n");
 		ft_bzero(buffer, BUFFER_SIZE + 1);
-//		printf("str = |%s|\n", str);
 		find_enter = NULL;
-//		printf("*** malloc 1 ****\n");
 		str = (char *) malloc(BUFFER_SIZE + 1);
 		if (!str)
 			return (NULL);
@@ -41,20 +37,22 @@ char 	*buffer_preproc(char *buffer, int *len_str)
 	}
 
 	//3
-//	printf("CASE 3\n");
-	*len_str = &buffer[BUFFER_SIZE] - find_enter - 1;
-//	printf("*len_str = %d\n", *len_str/* + BUFFER_SIZE + 1*/);
-//	printf("*** malloc 2 ****\n");
+	find_end = ft_memchr(buffer, '\0', BUFFER_SIZE);
+	if (find_end)
+	{
+		*len_str = find_end - &buffer[0] - 1;
+	}
+	else
+		*len_str = &buffer[BUFFER_SIZE] - find_enter - 1;
+
 	str = (char *) malloc(*len_str + BUFFER_SIZE + 1);
 	if (!str)
 	{
-//		printf("return NULL 1\n");
 		return (NULL);
 	}
 	ft_bzero(str, *len_str + BUFFER_SIZE + 1);
 
 	ft_memcpy(str, find_enter + 1, *len_str);
-//	printf("str = %s\n", str);
 
 	find_enter = (NULL);
 
@@ -62,15 +60,17 @@ char 	*buffer_preproc(char *buffer, int *len_str)
 	find_enter = (char *)ft_memchr(str, '\n', BUFFER_SIZE);
 	if (find_enter)
 	{
-//		printf("CASE 4\n");
 		find_enter = (char *)ft_memchr(buffer, '\n', BUFFER_SIZE);
-		*find_enter = 's';
+
+		len = find_enter - buffer + 1;
+
+		ft_memcpy(buffer, buffer + len, BUFFER_SIZE - len);
+		buffer[BUFFER_SIZE - len] = '\0';
+		buffer[BUFFER_SIZE] = '?';
 	}
 	else
 		ft_bzero(buffer, BUFFER_SIZE + 1);
 
-//	printf("buffer (pp-e) = %s\n", buffer);
-//	printf("return str (preproc)\n");
 	return (str);
 }
 
@@ -85,39 +85,29 @@ char	*get_next_line(int fd)
 	int			r;
 
 	char 		*find_enter;
+	int 		len;
 
-//	printf("\n*** Start gnl ***\n");
 	sbf = 0;
 	i = 0;
 
-
-//	printf("buffer 1 = |%s|\n", buffer);
 	str_buff = buffer_preproc(buffer, &sbf);
 	if (!str_buff)
 		return (NULL);
-//	printf("buffer 2 = |%s|\n", buffer);
 
 
-	while (!buffer[BUFFER_SIZE])
+	while (buffer[BUFFER_SIZE] != '!' && buffer[BUFFER_SIZE] != '?')
 	{
-//		printf("str_buff b = |%s|\n", str_buff);
 		r = read(fd, buffer, BUFFER_SIZE);
 		if (r > 0)
 		{
 			ft_memcpy(&str_buff[BUFFER_SIZE * i + sbf], buffer, r);
-//			printf("buffer = |%s|\n", buffer);
-//			printf("str_buff a = |%s|\n", str_buff);
-//			printf("r = %d\n", r);
 			if (r < BUFFER_SIZE)
 			{
-//				printf("BP 3\n");
-//				buffer[BUFFER_SIZE] = '!';
-//				buffer[r] = '\n';
 				if (ft_memchr(buffer, '\n', BUFFER_SIZE))
 				{
-//					printf("BP 4\n");
 					find_enter = ft_memchr(str_buff, '\n',
-										   BUFFER_SIZE * i + sbf	+ 1);
+										   BUFFER_SIZE * (i + 1) + sbf + 1);
+
 					*(find_enter + 1) = '\0';
 
 					return (str_buff);
@@ -129,7 +119,7 @@ char	*get_next_line(int fd)
 			i++;
 			if (ft_memchr(buffer, '\n', BUFFER_SIZE))
 			{
-//				printf("BP 4\n");
+
 				find_enter = ft_memchr(str_buff, '\n',
 									   BUFFER_SIZE * i + sbf	+ 1);
 				*(find_enter + 1) = '\0';
@@ -137,14 +127,8 @@ char	*get_next_line(int fd)
 				return (str_buff);
 			}
 
-
-//			printf("*** realloc ****\n");
-//			printf("oldlen = %d\n", BUFFER_SIZE * i + sbf + 1);
-//			printf("newlen = %d\n", BUFFER_SIZE * (i + 1) + sbf + 1);
-
 			str_buff = ft_realloc(str_buff, BUFFER_SIZE * i + sbf + 1,
 								  BUFFER_SIZE * (i + 1) + sbf + 1);
-//			str_buff = realloc(str_buff,BUFFER_SIZE * (i + 1) + sbf + 1);
 			if (!str_buff)
 				return (NULL);
 
@@ -155,8 +139,6 @@ char	*get_next_line(int fd)
 		{
 			if (r == -1)
 			{
-//				printf("BP 1\n");
-//				printf("free 1\n");
 				free(str_buff);
 				return (NULL);
 			}
@@ -166,30 +148,20 @@ char	*get_next_line(int fd)
 				buffer[BUFFER_SIZE] = '!';
 				if (*str_buff)
 				{
-//					printf("BP 555\n");
 					return (str_buff);
 				}
-//				printf("BP 2\n");
-//				printf("free 2\n");
 				free(str_buff);
 				return (NULL);
-
 			}
 		}
-
-
-
-
-
-
-
 	}
 
 
 //	ret
+	find_enter = ft_memchr(buffer, '\n', BUFFER_SIZE);
+	len = find_enter - &buffer[0] + 1;
+	ft_memcpy(str_buff, buffer, len);
+	str_buff[len] = '\0';
 
-
-
-	printf("BP 666\n");
 	return (str_buff);
 }

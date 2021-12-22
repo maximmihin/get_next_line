1e8069a13172ab4b48a2bf114bb805d2bead7341
Georgette Radagast <gradagas@un-c5.kzn.21-school.ru>
2021-12-22 04:27:30 +0300

    snap-1

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -92,3 +92,80 @@ char	*ft_realloc(char *buff, int oldlen, int newlen)
 		return (box);
 	}
 }
+
+
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*arr;
+	size_t	i;
+	size_t	slen;
+
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start > slen)
+		len = 0;
+	else if (len >= slen)
+		len = slen - start;
+	arr = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (len && s[start] && start < slen)
+	{
+		arr[i] = s[start];
+		i++;
+		start++;
+		len--;
+	}
+	arr[i] = 0;
+	return (arr);
+}
+
+char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	char	*arr;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (0);
+	i = ft_strlen(s1) + ft_strlen(s2);
+	arr = (char *)malloc(sizeof (*s1) * (i + 1));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		arr[i] = s1[i];
+		i++;
+	}
+	while (*s2)
+	{
+		arr[i] = *s2++;
+		i++;
+	}
+	arr[i] = 0;
+	free (s1);
+	return (arr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (unsigned char)c && *s != '\0')
+		s++;
+	if (*s == (unsigned char)c)
+		return ((char *)s);
+	else
+		return (NULL);
+}
